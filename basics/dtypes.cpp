ea9ca8495f7edde0bee0722e336d93ef32cf14a0
Nikhil Ranjan <115285383+Dcoder-Nikhil@users.noreply.github.com>
2025-06-09 07:33:15 +0000

    dtypes

---
+++ basics/dtypes.cpp
@@ -0,0 +1,20 @@
+// defining vaaibels with daa types and fromat sepcifiers
+#include<iostream>
+#include<stdlib.h>
+
+int main(){
+
+    // defning data types
+    int a = 10;
+    char ch = 'N';
+    float f = 10.23;
+    double db = 1.45236;
+
+    std::cout<< a <<"%d\n";
+    std::cout<< ch <<"%c\n";
+    std::cout<< f <<"%f\n";
+    std::cout<< db<<"%lf\n";
+
+    
+    return 0;
+}
