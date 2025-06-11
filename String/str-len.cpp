3d2eb9918f2fc2f1fa99209ccd52b76f5c662690
Nikhil Ranjan <115285383+Dcoder-Nikhil@users.noreply.github.com>
2025-06-11 04:31:25 +0000

    String length function

---
+++ String/str-len.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    //lets declare a string having a sentance
+    string sen ="Hey, I am Dcoder Nikhil, a Data Scientist";
+    
+    // now there are two in-built function in string to get the length of the string
+
+    cout<<"\nThe length method: "<< sen.length();
+
+    cout<<"\nThe Size method: "<<sen.size();
+    return 0;
+}
